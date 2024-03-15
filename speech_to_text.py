00aeae04e39e67da541817d4d059fb81d1d5e097
ryzqi <128199788+ryzqi@users.noreply.github.com>
2024-03-15 16:12:43 +0800

    Update speech_to_text.py
    类封装

--- speech_to_text.py
+++ speech_to_text.py
@@ -3,32 +3,29 @@ from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 from datasets import load_dataset
 
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "openai/whisper-large-v3"
-
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-)
-model.save_pretrained('./whisper-large-v3')
-model.to(device)
-
-processor = AutoProcessor.from_pretrained(model_id)
-
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model=model,
-    tokenizer=processor.tokenizer,
-    feature_extractor=processor.feature_extractor,
-    max_new_tokens=128,
-    chunk_length_s=30,
-    batch_size=16,
-    return_timestamps=True,
-    torch_dtype=torch_dtype,
-    device=device,
-)
-def speech_to_text(mp3):
-    result = pipe(mp3, return_timestamps=False)
-    return result['text']
-print(speech_to_text('./a.mp3'))
+class Speech_To_Text:
+    def __init__(self) -> None:
+        self.model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        "./whisper-large-v3", low_cpu_mem_usage=True, use_safetensors=True, device_map='cuda')
+        
+        self.processor = AutoProcessor.from_pretrained('./whisper-large-v3')
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+        
+        self.pipe = pipeline(
+        "automatic-speech-recognition",
+        model=self.model,
+        tokenizer=self.processor.tokenizer,
+        feature_extractor=self.processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=self.torch_dtype,
+        device=self.device,
+        )
+        
+    def speech_to_text(self, mp3):
+        result = self.pipe(mp3, return_timestamps=False)
+        return result['text']
